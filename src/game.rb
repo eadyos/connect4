91fdf9a5389da5eaf65910abd5fc786153d6a661
Steve Eady <seady@delorean.local>
2017-05-15 13:15:42 -0400

    Implemented Basic Computer Player

--- src/game.rb
+++ src/game.rb
@@ -1,8 +1,157 @@
 class Game 
 
+    @width
+    @height
+    @columns
+    @@empty = ' '
+
+    def initialize()
+        @width = 7
+        @height = 6
+        @columns = []
+        @width.times do 
+            @columns << Array.new(@height, @@empty)
+        end
+    end
+
+    def show()
+        puts state_as_text()
+    end
+
+    def state_as_text()
+        text = ""
+        (0...@height).reverse_each{ |idx|
+            row = []
+            @columns.each{|column|
+                val = column[idx]
+                row << val
+            }
+            text += "|#{row.join('|')}|\n" 
+        }
+        text += "---------------\n"
+        text += "|0|1|2|3|4|5|6|\n"
+        return text
+    end
+
+    def add_token_to_column(token, column)
+        row = @columns[column]
+        if row.include?(@@empty) 
+            row[row.index(@@empty)] = token
+        else
+            false
+        end
+    end
+
+    def take_computer_turn(computer_token, player_token)
+        if potential_column_neighbor_counts(computer_token)[3]
+            add_token_to_column(computer_token,
+                potential_column_neighbor_counts(computer_token)[3].sample)
+        elsif potential_column_neighbor_counts(player_token)[3]
+            add_token_to_column(computer_token,
+                potential_column_neighbor_counts(player_token)[3].sample)
+        else
+            add_token_to_column(computer_token,
+                potential_column_neighbor_counts(computer_token).max_by{|k,v| k}[1].sample)
+        end
+    end
+
+    def potential_column_neighbor_counts(token)
+        column_neighbor_counts(token, true)
+    end
+
+    def column_neighbor_counts(token, potential = false)
+        column_neighbor_count = Hash.new
+        (0...@width).each{|i|
+            if(potential)
+                j = @columns[i].index(@@empty)
+            else
+                j = @columns[i].rindex(token)
+            end
+            if j != nil
+                count = neighbor_count(token, i, j)
+            else
+                count = 0
+            end
+            if column_neighbor_count[count] == nil
+                column_neighbor_count[count] = []
+            end
+            column_neighbor_count[count] << i
+        }
+        return column_neighbor_count
+    end
+
+    def neighbor_count(token, i, j)
+        neighbor_counts = []
+        #down
+        col = i
+        row = j - 1
+        count = 0
+        while row >= 0 && @columns[col][row] == token do
+            count += 1
+            row -= 1
+        end
+        neighbor_counts << count
+        #across
+        col = i + 1
+        row = j
+        count = 0
+        while col < @width && @columns[col][row] == token do
+            count += 1
+            col += 1
+        end
+        col = i - 1
+        while col >= 0 && @columns[col][row] == token do
+            count += 1
+            col -= 1
+        end
+        neighbor_counts << count
+        #upright and downleft
+        #upright
+        col = i + 1
+        row = j + 1
+        count = 0
+        while row < @height && col < @width && @columns[col][row] == token do
+            count += 1
+            row += 1
+            col += 1
+        end
+        #downleft
+        col = i - 1
+        row = j - 1
+        while row >= 0 && col >= 0 && @columns[col][row] == token do
+            count += 1
+            row -= 1
+            col -= 1
+        end
+        neighbor_counts << count
+        #upleft and downright
+        #upleft
+        col = i - 1
+        row = j + 1
+        count = 0
+        while row < @height && col >= 0 && @columns[col][row] == token do
+            count += 1
+            row += 1
+            col -= 1
+        end
+        #downright
+        col = i + 1
+        row = j - 1
+        while row >= 0 && col < @width && @columns[col][row] == token do
+            count += 1
+            row -= 1
+            col += 1
+        end
+        neighbor_counts << count
+        neighbor_counts.max
+    end    
+
+    def is_won?(token)
+        column_neighbor_counts(token).keys.max >= 3
+    end
     
-
-
-
+    def is_draw?
+        !@columns.join.include?(@@empty)
+    end        
 
 end
--- test/tc_game.rb
+++ test/tc_game.rb
@@ -1,15 +1,70 @@
 require_relative "../src/game"
-require "test/unit"
+require "minitest/autorun"
 
-class TC_Game < Test::Unit::TestCase
+class TC_Game < Minitest::Test
 
     def test_create
-        assert_not_nil(Game.new)
+        refute_nil(Game.new)
     end
 
-    def test_show
+    def test_new_state_as_text
         game = Game.new
-        assert_match(game.show, /(\| \| \| \| \| \| \| \|\n){6}/)
+        assert_match(/(\| \| \| \| \| \| \| \|\n){6}/,
+            game.state_as_text)
+    end
+ 
+    def test_game_not_won
+        game = Game.new
+        assert_equal(game.is_won?("1"), false)
     end
 
-end
+    def test_game_won_diagnol_up
+        game = Game.new
+        game.add_token_to_column("1",1)
+        game.add_token_to_column("2",2)
+        game.add_token_to_column("1",2)
+        game.add_token_to_column("2",3)
+        game.add_token_to_column("2",3)
+        game.add_token_to_column("1",3)
+        game.add_token_to_column("2",4)
+        game.add_token_to_column("2",4)
+        game.add_token_to_column("2",4)
+        game.add_token_to_column("1",4)
+        assert_equal(game.is_won?("1"), true)
+    end
+    
+    def test_game_won_diagnol_down
+        game = Game.new
+        game.add_token_to_column("2",1)
+        game.add_token_to_column("2",1)
+        game.add_token_to_column("2",1)
+        game.add_token_to_column("1",1)
+        game.add_token_to_column("2",2)
+        game.add_token_to_column("2",2)
+        game.add_token_to_column("1",2)
+        game.add_token_to_column("2",3)
+        game.add_token_to_column("1",3)
+        game.add_token_to_column("1",4)
+        assert_equal(game.is_won?("1"), true)
+    end
+
+    def test_game_won_horizontal
+        game = Game.new
+        game.add_token_to_column("1",1)
+        game.add_token_to_column("1",2)
+        game.add_token_to_column("1",3)
+        game.add_token_to_column("1",4)
+        assert_equal(game.is_won?("1"), true)
+    end
+    
+    def test_game_won_vertical
+        game = Game.new
+        game.add_token_to_column("1",1)
+        game.add_token_to_column("1",1)
+        game.add_token_to_column("1",1)
+        game.add_token_to_column("1",1)
+        assert_equal(game.is_won?("1"), true)
+    end
+
+
+end 
