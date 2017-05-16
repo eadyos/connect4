917454bd4387bb27da1b10e7511e5bc29b9718e0
Steve Eady <seady@delorean.local>
2017-05-16 11:28:54 -0400

    disabled command line runner for production

--- lib/main.rb
+++ lib/main.rb
@@ -1,39 +1,39 @@
-require_relative 'game'
-require "highline/import"
+# require_relative 'game'
+# require "highline/import"
 
 
-player1 = "1"
-player2 = "2"
-players = [player1, player2]
-turn = 0
+# player1 = "1"
+# player2 = "2"
+# players = [player1, player2]
+# turn = 0
 
-game = Game.new
+# game = Game.new
 
-player_count = ask "One or two player game? (1, 2): "
+# player_count = ask "One or two player game? (1, 2): "
 
-loop do
-    game.show()
-    currentPlayer = players[turn % players.size]
-    otherPlayer = players[(turn + 1) % players.size]
-    if turn % 2 == 1 && player_count == '1'
-        game.take_computer_turn(currentPlayer, otherPlayer)
-        turn += 1
-    else
-        column = ask "Player #{currentPlayer}, Select Column: "
-        if game.add_token_to_column(currentPlayer, column.to_i)
-            turn += 1
-        else
-            puts "Column is full.  Try again"
-        end
-    end
+# loop do
+#     game.show()
+#     currentPlayer = players[turn % players.size]
+#     otherPlayer = players[(turn + 1) % players.size]
+#     if turn % 2 == 1 && player_count == '1'
+#         game.take_computer_turn(currentPlayer, otherPlayer)
+#         turn += 1
+#     else
+#         column = ask "Player #{currentPlayer}, Select Column: "
+#         if game.add_token_to_column(currentPlayer, column.to_i)
+#             turn += 1
+#         else
+#             puts "Column is full.  Try again"
+#         end
+#     end
     
-    break if game.is_draw? || game.is_won?(currentPlayer)
-end
+#     break if game.is_draw? || game.is_won?(currentPlayer)
+# end
 
-game.show()
-currentPlayer = players[(turn -1) % players.size]
-if game.is_draw?
-    puts "The game is a draw"
-else
-    puts "The game is over.  Player #{currentPlayer} wins!"
-end
+# game.show()
+# currentPlayer = players[(turn -1) % players.size]
+# if game.is_draw?
+#     puts "The game is a draw"
+# else
+#     puts "The game is over.  Player #{currentPlayer} wins!"
+# end
